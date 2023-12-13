112dfc8b1a0f437c8bebdf4cb1bb0fd1d646c931
Arthur-N42 <arthur.nz.ju@gmail.com>
2023-12-13 16:37:55 +0100

    nicuuu

--- shell.sh
+++ shell.sh
@@ -2,13 +2,22 @@ if [ $# -lt  2 ] ; then
     echo "Erreur : deux arguments au moins sont nécessaires."
     exit 1
 elif [ ! -f "$1" ] || [ "${1##*.}" != "csv" ] ; then
-    echo "Erreur : premier argument n'est pas un fichier csv."
+    echo "Erreur : Le premier argument n'est pas un fichier csv."
     exit 1
 fi
 
+# Erreur trouvee : il manquait un espace "-h"ici]
+
 for i in $* ; do
-    if [ "$i" = "-h"] ; then
+    if [ "$i" = "-h" ] ; then
         echo "oui"
         exit 1
+    else
+    if [ -d "temp" ]; then
+        echo "Le dossier 'test' existe."
+    else
+        mkdir temp
+    fi
     fi
 done
+
