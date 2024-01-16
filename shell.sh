d77c33c6bdbcc2c566a184ac929dbeb7e802039f
Arthur-N42 <arthur.nz.ju@gmail.com>
2024-01-16 15:16:09 +0100

    traitements d1 d2 l corrects

--- shell.sh
+++ shell.sh
@@ -59,23 +59,21 @@ if [ $d1 = 1 ]; then
     # NR > 1 ignore la ligne 1 
     # Utilise un dictionnaire et incremente le nombre de trajet pour un conducteur si la paire (ID - Conducteur) n'a pas ete "ajoutee"
     # Trie dans l'ordre decroissant et affiche les 10 premiers
-    time awk -F';' 'NR > 1 { if (!added[$1, $6]) { trips[$6]++ } added[$1, $6]++ } END { for (driver in trips) print driver, trips[driver] }' $1 | sort -k3 -nr | head -10
+    time mawk -F';' 'NR > 1 { if (!added[$1, $6]) { trips[$6]++ } added[$1, $6]++ } END { for (driver in trips) print driver, trips[driver] }' $1 | sort -k3 -nr | head -10 # ~4.3s
 fi  
 
 if [ $d2 = 1 ]; then
-    echo "cool"
-    time awk -F';' 'NR > 1 { trips[$6]+=$5 } END { for (driver in trips) print driver, trips[driver] }' $1 | sort -k3 -nr | head -10 # ~2.2s exec
+    
+    time LC_NUMERIC="C" awk -F';' 'NR > 1 { trips[$6]+=$5 } END { for (driver in trips) print driver, trips[driver] }' $1 | sort -k3 -nr | head -10 # ~2.2s exec
 fi
 
 if [ $l = 1 ]; then
-    echo "cool"
     #A tester
-    time awk -F';' 'NR > 1 { trips[$1]+=$5 } END { for (distance in trips) print distance, trips[distance] }' $1 | sort -k3 -nr | head -10 # 
+    time LC_NUMERIC="C" awk -F';' 'NR > 1 { trips[$1]+=$5 } END { for (distance in trips) print distance, trips[distance] }' $1 | sort -k2 -nr | head -10 # ~4.6s
     
 fi
 
 if [ $t = 1 ]; then
-    echo "cool"
     gcc -o traitements main.c
     ./traitements -t
 fi
